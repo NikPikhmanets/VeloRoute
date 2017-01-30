d5b64aacf9e9e44f2069c05aeb39a4cce66d17af
Ivan Sierhieienko <cosinoos81@gmail.com>
2017-01-30 23:30:16 +0200

    change splash

--- app/src/main/java/com/nikpikhmanets/veloroute/SplashActivity.java
+++ app/src/main/java/com/nikpikhmanets/veloroute/SplashActivity.java
@@ -1,6 +1,7 @@
 package com.nikpikhmanets.veloroute;
 
 import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,9 +10,14 @@ public class SplashActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, MainActivity.class));
+                finish();
+            }
+        }, 2000);
 
-        Intent intent = new Intent(this, MainActivity.class);
-        startActivity(intent);
-        finish();
     }
 }
