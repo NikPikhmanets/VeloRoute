4b4ec2aba9520fc06e410467a0e9b1b29f427c20
NikPikhmanets <nik.pikhmanets@gmail.com>
2017-02-01 00:57:35 +0200

    add icon, change splash

--- app/src/main/java/com/nikpikhmanets/veloroute/SplashActivity.java
+++ app/src/main/java/com/nikpikhmanets/veloroute/SplashActivity.java
@@ -1,9 +1,8 @@
 package com.nikpikhmanets.veloroute;
 
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
 
@@ -11,13 +10,12 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
+//        new Handler().postDelayed(new Runnable() {
+//            @Override
+//            public void run() {
                 startActivity(new Intent(SplashActivity.this, MainActivity.class));
                 finish();
-            }
-        }, 2000);
-
+//            }
+//        }, 2000);
     }
 }
